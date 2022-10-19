32fc7e955f20fb302a1a986f498fc410e763dbef
imran-rassi <imran.rassi.2020@gmail.com>
2022-10-18 23:04:08 -0400

    added changes to script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,10 +2,27 @@ const searchBtn = document.getElementById('search-btn')
 const recipeList = document.getElementById('recipe');
 const recipeDetailContent = document.querySelector('recipe-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
-const spoonApiUrl = "https://api.spoonacular.com/recipes/complexSearch?titleMatch=%recipeCriteria%&number=50&apiKey=%APIKEY%";
+const spoonApiUrl = 'https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=API-KEY';
+const spoonApikey = '561cea46f3664faa9a4dd071c85d058f'
 
 //eventlisteners
 searchBtn.addEventListener('click', getRecipeList);
+
+async function getRecipeAPIdata(recipeUrl) {
+    // console.log(" >>>>>>  inside getRecipeAPIdata  >>>>>>>>>>>>>>>");
+    const response = await fetch(recipeUrl);
+    const jsonRecipe = await response.json();
+    
+    displayRecipeData(jsonRecipe); 
+
+    
+    function displayRecipeData(recipeData) {
+        console.log(" >>> inside displayRecipeSearchError >>>>>>>>>>>>");
+    
+        console.log("recipeData", recipeData);
+    
+        $("#recipeName").text(recipeData.title);
+    }
 
 //get recipe list that matches with the Name
 function getRecipeList(){
@@ -25,11 +42,26 @@ function getRecipeList(){
 
 }
 
-const searchBtn2 = document.getElementById('search-btn2')
-const DrinkList = document.getElementById('drink');
-const drinkDetailContent = document.querySelector('drink-details-content');
-const drinkCloseBtn = document.getElementById('drink-close-btn');
-const cocktailUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+// // // function getRandomcocktail() {
+// // //     console.log("getRandomcocktail");
+    
+let drinkId=715573;
+ const cocktailUrl = "www.thecocktaildb.com/api/json/v1/1/lookup.php?i=%drinkId%";
+var drinkUrl = cocktailUrl.replace("%drinkId%", drinkId);
+const drinkUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+getDrinkAPIdata(drinkUrl)
+then(response => {
+gatherDrink(drinkData);
+displayDrinkforSelection();
+console.log('yay from setDrinkCriteria');
+})
 
-//eventlisteners
-searchBtn2.addEventListener('click', getDrinkList);
+.catch(error => {
+console.log('error!');
+console.error(error)
+});
+
+        
+};
+
+getRandomcocktail();
